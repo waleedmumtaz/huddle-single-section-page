e056574f46505892387e0ab76020b9b3cc3d693d
Waleed <waleedthedev@gmail.com>
2022-01-02 01:05:39 +0500

    Setup up project according to the style guides

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,21 @@
 
 </script>
 
-<main>
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link
+    rel="preconnect"
+    href="https://fonts.gstatic.com"
+    crossorigin="anonymous"
+  />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@400;600&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
 
-</main>
-
+<main />
+  
 <style>
 
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,24 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cstm: {
+          primary: {
+            violet: 'hsl(257, 40%, 49%)',
+            'soft-magenta': 'hsl(300, 69%, 71%)',
+          },
+        },
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        'open-sans': ['Open Sans', 'sans-serif'],
+      },
+    },
   },
 
   plugins: [],
-};
+}
 
-module.exports = config;
+module.exports = config
